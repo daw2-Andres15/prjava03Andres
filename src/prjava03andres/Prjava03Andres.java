5b4b31d69183601d503e3b293a3f99c26abd063f
ubu2 <ubu2@m-ubu2>
2015-05-13 18:32:15 +0200

    Versió 0.7-C del projecte prjava03Andres. Branca master modificada.

--- src/prjava03andres/Prjava03Andres.java
+++ src/prjava03andres/Prjava03Andres.java
@@ -7,6 +7,7 @@ package prjava03andres;
 import java.awt.*;        // using AWT containers and components 
 import java.awt.event.*;  // using AWT events and listener interfaces 
 import java.io.*;
+import java.sql.*;
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava03Andres extends Frame 
       implements ActionListener, WindowListener { 
